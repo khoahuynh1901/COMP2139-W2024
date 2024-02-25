471f9412faac247ceae6641e6a1955c8ead038cc
khoa huynh <khoahuynh1901@gmail.com>
2024-02-24 22:50:16 -0500

    Assignment 1 updated

---
+++ Assginment 1/GBC_Travel-Group53/Models/Domain/Person.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GBC_Travel_Group53.Models.Domain
+{
+    public class Person
+    {
+		[Key]
+		[Required]
+        public string PassportNumber { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public string Phone { get; set; }
+		[Required]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Destination is required")]
+		public string Destination { get; set; }
+
+		[Display(Name = "Check In")]
+		[Required(ErrorMessage = "Check-in date is required")]
+		public DateTime CheckIn { get; set; }
+
+		[Display(Name = "Check Out")]
+		[Required(ErrorMessage = "Check-out date is required")]
+		public DateTime CheckOut { get; set; }
+
+		[Required(ErrorMessage = "Number of rooms is required")]
+		[Range(1, int.MaxValue, ErrorMessage = "Please select at least one room")]
+		public int Rooms { get; set; }
+
+		[Required(ErrorMessage = "Number of adults is required")]
+		[Range(1, int.MaxValue, ErrorMessage = "Please select at least one adult")]
+		public int Adults { get; set; }
+
+		[Required(ErrorMessage = "Number of children is required")]
+		[Range(0, int.MaxValue, ErrorMessage = "Please enter a valid number of children")]
+		public int Children { get; set; }
+
+
+
+	}
+}
---
+++ Assginment 1/GBC_Travel-Group53/Program.cs
@@ -0,0 +1,36 @@
+using GBC_Travel_Group53.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GBC_Travel_Group53
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Conn")));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
